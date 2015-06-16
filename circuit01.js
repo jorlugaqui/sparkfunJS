4a39843bd76f473e4f07cb40b52f20187aed1a7e
jorlugaqui <jorlugaqui@gmail.com>
2015-06-15 22:46:22 -0500

    Add some comments.

--- circuit01.js
+++ circuit01.js
@@ -1,7 +1,11 @@
+// Curcuit 01 from http://cdn.sparkfun.com/datasheets/Kits/SFE-SIK-RedBoard-Guide-Version3.0-Online.pdf
+// Blinkig an LED.
 var five = require("johnny-five");
 var board = new five.Board();
 
 board.on("ready", function() {
+  // Led connected to digital pin 13.
   var led = new five.Led(13);
+  // Blink every half of second.
   led.blink(500);
 });
--- circuit02.js
+++ circuit02.js
@@ -1,8 +1,11 @@
+// Curcuit 02 from http://cdn.sparkfun.com/datasheets/Kits/SFE-SIK-RedBoard-Guide-Version3.0-Online.pdf
+// Reading from a potentiometer
 var five = require("johnny-five");
 var board = new five.Board();
 
 board.on("ready", function() {
 
+  // Middle pin connected on A0 input
   var potentiometer = new five.Sensor({
     pin: "A0",
     freq: 250
@@ -12,8 +15,10 @@ board.on("ready", function() {
     pot: potentiometer
   });
 
+  // LED connected to digital pin 13.
   var led = new five.Led(13);
 
+  // Update freq of the blink (LED) with the middle pin input connected.
   potentiometer.on("change", function() {
     console.log(this.value, this.raw);
     led.blink(this.value);
